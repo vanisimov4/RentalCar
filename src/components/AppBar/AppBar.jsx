5843ac68ccf61447d7426fa653a763a923a1763f
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2025-09-12 15:59:36 +0300

    refactoringNavigation

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,25 @@
-import { Navigation } from '../Navigation/Navigation';
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 
-import css from './AppBar.module.css';
+import css from "./AppBar.module.css";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export default function AppBar() {
   return (
     <header className={css.header}>
-      <Navigation />
+      <p className={css.logo}>RentalCar</p>
+      <nav className={css.nav}>
+        <NavLink className={buildLinkClass} to="/">
+          Home
+        </NavLink>
+
+        <NavLink className={buildLinkClass} to="/catalog">
+          Catalog
+        </NavLink>
+      </nav>
     </header>
   );
 }
