a15aa2dc367d547ff96af89c38d60ff9a2eaa8e5
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-09-14 20:53:15 +0300

    resolveProblenWithDeploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import App from './components/Apps/App.jsx';
+import App from './components/App/App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
